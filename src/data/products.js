26def56d3034f535dd1604c93266cdee4931d212
Samueltwds <samueltwd61@gmail.com>
2026-06-09 15:39:08 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 1,
     name: "Fone Apolo Pro",
-    price: 198.99,
+    price: 49.99,
     image:
       "https://down-ph.img.susercontent.com/file/cn-11134207-7r98o-ly4is7pijzze6a",
   },
